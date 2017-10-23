7350c4ddd3684e6543b85221d9df4b8b2dc36f19
mtaulty <oliver.schnabel@volkswagen.de>
2017-10-23 15:30:15 +0100

    Added UWP 2D and 3D demo for session 1

---
+++ Session1_Bootstrapping/UWP2Dand3DApp/App1/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using HolographicApp1;
+using System;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      this.InitializeComponent();
+    }
+
+    async void OnButtonClick(object sender, RoutedEventArgs e)
+    {
+      // We only go 2D->3D right now, we could change that later on if
+      // we wanted to.
+      var newView = CoreApplication.CreateNewView(new AppViewSource());
+
+      ApplicationView appView = null;
+
+      await newView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+        () =>
+        {
+          appView = ApplicationView.GetForCurrentView();
+          CoreWindow.GetForCurrentThread().Activate();
+        }
+      );
+      await ApplicationViewSwitcher.TryShowAsStandaloneAsync(appView.Id);
+    }
+  }
+}
---
+++ Session1_Bootstrapping/UWP2Dand3DApp/App1/Program.cs
@@ -0,0 +1,21 @@
+﻿using App1;
+using System;
+using Windows.ApplicationModel.Core;
+
+namespace HolographicApp1
+{
+  /// <summary>
+  /// Windows Holographic application using SharpDX.
+  /// </summary>
+  internal class Program
+  {
+    /// <summary>
+    /// Defines the entry point of the application.
+    /// </summary>
+    [MTAThread]
+    private static void Main()
+    {
+      Windows.UI.Xaml.Application.Start(p => new App());
+    }
+  }
+}
